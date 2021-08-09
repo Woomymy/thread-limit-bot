8804c2e38ac7e0f8cb6dadd022a6d35e43610504
Woomy <woomy@woomy.be>
2021-08-09 13:43:54 +0200

    Handler: Set commands to global

--- src/commandhandler.ts
+++ src/commandhandler.ts
@@ -18,15 +18,14 @@ export default class CommandHandler {
     async init() {
         const commandsDir = `${dirname(import.meta.url.substring(7))}/commands`
         const files = (await readdir(commandsDir)).filter(f => f.endsWith(".js"));
+        await this.client.application.commands.set([])
         files.forEach(async file => {
             const command = await import(`${commandsDir}/${file}`);
             const def = command.default
             const cmd: Command = new def();
             this.commands.set(cmd.name, cmd);
+            this.client.application.commands.create(cmd.data)
             console.log(`Handler: Loaded cmd ${cmd.name}`)
-            this.client.guilds.cache.forEach(async guild => {
-                guild.commands.create(cmd.data);
-            })
         })
     }
     async exec(inter: CommandInteraction, threadCache: Map<string, number>) {
