916bde12e4c1348f17e3312765a30867907b80fb
Woomy <woomy@woomy.be>
2021-08-09 20:01:08 +0200

    Nthreads: HOTFIX: Fix execution in guilds

--- src/commands/nthreads.ts
+++ src/commands/nthreads.ts
@@ -16,11 +16,14 @@ export default class extends Command {
         })
     }
     async run(inter: CommandInteraction, threadCache: Map<string, number>) {
+        if (inter.guild) {
+            inter.reply("Cette command ne peut pas être utilisée en messages privés!");
+            return;
+        }
         const user = inter.options.get("user")?.user ?? inter.user;
         const nthreads = threadCache.get(`${inter.guild.id}${user.id}`) || 0;
         inter.reply(
-            `${user.username} a créé ${nthreads == 0 ? "aucun" : nthreads} thread${nthreads > 1 ? "s" : ""
-            }`
+            `${user.username} a créé ${nthreads == 0 ? "aucun" : nthreads} thread${nthreads > 1 ? "s" : ""}`
         );
     }
 }
